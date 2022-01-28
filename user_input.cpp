77a9493d9fd286077632376b450d84ebfe7c0aac
code-ntj <98606236+code-ntj@users.noreply.github.com>
2022-01-28 23:09:21 +0530

    user-input
    user-input program in C++.

---
+++ user_input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+
+    cout<<"Enter a Number:";
+    cin>>number;
+    cout<<"Entered number is:"<<number;
+    return 0;
+}
